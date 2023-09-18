143ebf9e0c3a1fc5cb64a02e7882ca4715e833d0
jonghwan <subdiary@daum.net>
2023-09-18 14:50:59 +0900

    build

--- src/components/GoodsItem.vue
+++ src/components/GoodsItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="goods_item">
-        path : {{ path }}
-        <a :href="path+'GD_01_01_01'"><!-- 퍼블 확인용 링크 -->
+        device : {{ device }}
+        <a :href="'/'+device+'/GD_01_01_01'"><!-- 퍼블 확인용 링크 -->
             <span class="thumb">
                 <em><img v-bind:src="item.img" /></em>
                 <i v-if="item.option_icon">{{item.option_icon}}</i><!-- 23.04.18 관리자 등록 옵션 값 추가 -->
@@ -71,7 +71,7 @@ export default {
         isWish: false,
         isCart: false,
         optionActive: false,
-        path: String,
+        device: String
     },
     mounted() {
         /* 23.04.04 PC 사이즈 옵션 레이어 노출 관련 */
@@ -92,19 +92,7 @@ export default {
         /* //23.04.04 PC 사이즈 옵션 레이어 노출 관련 */
 
         /* 퍼블 확인용 링크 */
-        // if(window.location.hostname == 'formnj.github.io'){
-        //     this.path = '/cowell/';
-        // } else {
-        //     this.path = '/'
-        // }
-        path = window.location.pathname.split('/')[1];
-        console.log('aaa:',window.location.pathname);
-        if(path == 'pc' || path == undefined || path == '' || path == 'Guide'){
-            this.path = '/pc/';
-        }
-        if(path == 'mo'){
-            this.path = '/mo/';
-        }
+        this.device = device;
         /* //퍼블 확인용 링크 */
     },
     methods:{
